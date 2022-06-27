c7588a623a03fa46489073341d9c962ee4650011
Jia Jia <angersax@sina.com>
2022-06-27 22:12:23 +0800

    feat(forstmt): add support to visit for statement

--- src/main/java/com/yourorganization/maven/JavaLint.java
+++ src/main/java/com/yourorganization/maven/JavaLint.java
@@ -2,8 +2,9 @@ package com.yourorganization.maven;
 
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.expr.BinaryExpr;
-import com.github.javaparser.ast.stmt.IfStmt;
+import com.github.javaparser.ast.stmt.ForStmt;
 import com.github.javaparser.ast.stmt.Statement;
+import com.github.javaparser.ast.stmt.WhileStmt;
 import com.github.javaparser.ast.visitor.ModifierVisitor;
 import com.github.javaparser.ast.visitor.Visitable;
 import com.github.javaparser.utils.CodeGenerationUtils;
@@ -14,49 +15,35 @@ import java.nio.file.Paths;
 
 public class JavaLint {
     public static void main(String[] args) {
-        // JavaParser has a minimal logging class that normally logs nothing.
-        // Let's ask it to write to standard out:
         Log.setAdapter(new Log.StandardOutStandardErrorAdapter());
 
-        // SourceRoot is a tool that read and writes Java files from packages on a certain root directory.
-        // In this case the root directory is found by taking the root from the current Maven module,
-        // with src/main/resources appended.
         SourceRoot sourceRoot = new SourceRoot(CodeGenerationUtils.fileInPackageAbsolutePath("src/main/resources", "", ""));
-
-        // Our sample is in the root of this directory, so no package name.
         CompilationUnit cu = sourceRoot.parse("", "Loop.java");
 
-        Log.info("Test Loop");
+        Log.info("Running...");
 
         cu.accept(new ModifierVisitor<Void>() {
-            /**
-             * For every if-statement, see if it has a comparison using "!=".
-             * Change it to "==" and switch the "then" and "else" statements around.
-             */
             @Override
-            public Visitable visit(IfStmt n, Void arg) {
-                // Figure out what to get and what to cast simply by looking at the AST in a debugger! 
-                n.getCondition().ifBinaryExpr(binaryExpr -> {
-                    if (binaryExpr.getOperator() == BinaryExpr.Operator.NOT_EQUALS && n.getElseStmt().isPresent()) {
-                        /* It's a good idea to clone nodes that you move around.
-                            JavaParser (or you) might get confused about who their parent is!
-                        */
-                        Statement thenStmt = n.getThenStmt().clone();
-                        Statement elseStmt = n.getElseStmt().get().clone();
-                        n.setThenStmt(elseStmt);
-                        n.setElseStmt(thenStmt);
-                        binaryExpr.setOperator(BinaryExpr.Operator.EQUALS);
-                    }
-                });
+            public Visitable visit(ForStmt n, Void arg) {
+                if (n.getCompare().isPresent()) {
+                    n.getCompare().get().ifBinaryExpr(binaryExpr -> {
+                        if (binaryExpr.getOperator() == BinaryExpr.Operator.GREATER
+                                || binaryExpr.getOperator() == BinaryExpr.Operator.GREATER_EQUALS
+                                || binaryExpr.getOperator() == BinaryExpr.Operator.LESS
+                                || binaryExpr.getOperator() == BinaryExpr.Operator.LESS_EQUALS) {
+                            binaryExpr.getRight().ifBinaryExpr(b -> {
+                                if (b.getOperator() == BinaryExpr.Operator.MINUS
+                                        || b.getOperator() == BinaryExpr.Operator.PLUS) {
+                                    Log.info("here");
+                                }
+                            });
+                        }
+                    });
+                }
                 return super.visit(n, arg);
             }
         }, null);
 
-        // This saves all the files we just read to an output directory.  
-        sourceRoot.saveAll(
-                // The path of the Maven module/project which contains the LogicPositivizer class.
-                CodeGenerationUtils.mavenModuleRoot(JavaLint.class)
-                        // appended with a path to "output"
-                        .resolve(Paths.get("output")));
+        Log.info("Completed.");
     }
 }
